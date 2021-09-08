ddc10bbb3a93c46f47c355ed49f482015714e88a
jkrosby51 <56936867+jkrosby51@users.noreply.github.com>
2021-09-08 09:46:15 -0400

    Create ObjectHeight.java

---
+++ ObjectHeight.java
@@ -0,0 +1,19 @@
+//h = 100 - 4.9(t^2) -- equation for height when dropped from 100m for t amount of time.
+import java.util.*;
+public class ObjectHight {
+    
+    public static void main(String[] args){
+        double time;
+        double height;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a time less than 4.5 seconds: ");
+        time = input.nextDouble();
+
+        height = 100 - 4.9 * (time*time);
+        System.out.println("The height of the object is: " + height);
+        
+
+
+    }
+}
